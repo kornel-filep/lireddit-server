63a405efc6261844a823d7e3cbd159310fde1761
Kornel Filep <kornel_filep@epam.com>
2021-03-02 22:34:27 +0100

    added scuffed error handling to user resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -38,18 +38,52 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    //this is dirty, consider using a validation library
+    if (options.username.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
+    if (options.password.length <= 8) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "length must be greater than 8",
+          },
+        ],
+      };
+    }
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
     });
-    await em.persistAndFlush(user);
-    return user;
+    try {
+      await em.persistAndFlush(user);
+    } catch (error) {
+      if (error.code === "23505") {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "The username has already been taken",
+            },
+          ],
+        };
+      }
+    }
+    return { user };
   }
 
   @Mutation(() => UserResponse)
